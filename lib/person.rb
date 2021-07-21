d5ef904553c1ba48b572b79ecd2364ba0360de2e
JoeMilius <joemilius@gmail.com>
2021-07-21 15:49:30 -0600

    commit tree cffbd5e007200780b3fd02d42ce6961fdae824cc

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,60 @@
-test
+require 'pry'
+class Person
 
+    attr_reader :name, :happiness
+    attr_accessor :bank_account, :hygiene
 
-test from john 
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+   
+    def happiness=(value)
+        if value > 10
+            @happiness = 10
+        elsif value < 0
+            @happiness = 0
+        else
+            @happiness = value
+        end
+    end
+
+    def hygiene=(value)
+        if value > 10
+            @hygiene = 10
+        elsif value < 0
+            @hygiene = 0
+        else
+            @hygiene = value
+        end
+    end
+
+    def happy?
+        if self.happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+    def clean?
+        if self.hygiene > 7
+            true
+        else
+            false
+        end
+    end
+
+    def get_paid
+        @amount = amount
+    end
+    
+    def get_paid
+        @amount
+        binding.pry
+    end
+
+end
+
